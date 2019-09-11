8baed07c3a3157c02bb70a532220f2d6ec4c7134
Jurgen Emanuels <jurgen.emanuels@cyberiam.com>
2019-09-11 21:47:54 +0200

    Router functions has been complted

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,6 +25,8 @@ export class NavComponent implements OnInit {
       },
       error => {
         this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -35,5 +38,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
